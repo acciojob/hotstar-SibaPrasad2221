25dfe18a70224d5e1f0eac1b912ca1e278679615
sahoo <sahoosibaprasad2221@gmail.com>
2024-01-04 22:36:52 +0530

    final op1

--- src/main/java/com/driver/repository/SubscriptionRepository.java
+++ src/main/java/com/driver/repository/SubscriptionRepository.java
@@ -3,6 +3,8 @@ package com.driver.repository;
 import com.driver.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription,Integer> {
 
 }
--- src/main/java/com/driver/repository/WebSeriesRepository.java
+++ src/main/java/com/driver/repository/WebSeriesRepository.java
@@ -1,7 +1,10 @@
 package com.driver.repository;
 
+import com.driver.model.SubscriptionType;
 import com.driver.model.WebSeries;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface WebSeriesRepository extends JpaRepository<WebSeries,Integer> {
 
